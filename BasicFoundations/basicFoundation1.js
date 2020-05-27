e606d01ffd800da7d4ec237d414965f050acc356
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-05-27 14:01:44 -0700

    Still working on problem 2, stopped due to class starting

--- BasicFoundations/basicFoundation1.js
+++ BasicFoundations/basicFoundation1.js
@@ -8,3 +8,15 @@ function returnAnArray(){
     return arr;
 }
 returnAnArray();
+
+// Get even 1000 - Write a function that would get the sum of all the even numbers from 1 to 1000.  You may use a modulus operator for this exercise.
+
+function sumOfAllEvenNumbers(){
+    for(var i = 1; i <= 1000; i++) {
+        if(i%2 == 0){
+            sum.push(i);
+            console.log(i);
+        }
+    }
+    
+}
