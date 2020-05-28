7bac9482d616155dc3104a49c10b46109e0cb550
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-05-27 17:56:20 -0700

    Question 6 solved

--- BasicFoundations/basicFoundation1.js
+++ BasicFoundations/basicFoundation1.js
@@ -59,6 +59,15 @@ findMax([6, 7, 2, 4, 9]);
 
 // 6. Find average - Given an array with multiple values, write a function that returns the average of the values in the array. (e.g. for [1,3,5,7,20] average is 7.2)
 
+function sumOfArray(arr) {
+    var sumOfArr = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sumOfArr += arr[i]/arr.length;
+    }
+    return sumOfArr;
+}
+sumOfArray([1,3,5,7,20]);
+
 
 // 7. Array odd - Write a function that would return an array of all the odd numbers between 1 to 50. (ex. [1,3,5, .... , 47,49]). Hint: Use 'push' method.
 
