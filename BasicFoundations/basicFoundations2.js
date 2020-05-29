8374b54539fbed7d0e499bb12fe9c464d9777b4a
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-05-29 16:08:01 -0700

    Problem 7 completed

--- BasicFoundations/basicFoundations2.js
+++ BasicFoundations/basicFoundations2.js
@@ -38,7 +38,17 @@ doubleValue(arr);
 
 
 // 7. Increment the Seconds - Given an array of numbers arr, add 1 to every other element, specifically those whose index is odd (arr[1], arr[3], arr[5], etc).  Afterward, console.log each array value and return arr.
-
+var arr = [1,2,3,4,5];
+function everyOther(arr){
+    for(var i = 0; i < arr.length; i++) {
+        if(i % 2 == 1){
+            arr[i] = arr[i] + 1;
+            console.log(arr);
+        }
+    }
+    return arr;
+}
+everyOther(arr);
 
 // 8. Previous Lengths - You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
 
