7b1dcd7f1c00a5ba9925247ea9f0fff26077e7b0
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-02 22:42:38 -0700

    TAKING A BREAK!

--- python_stack/python/fundamentals/for_loop_basic2.py
+++ python_stack/python/fundamentals/for_loop_basic2.py
@@ -14,16 +14,25 @@
 # Example: count_positives([-1,1,1,1]) changes the original list to [-1,1,1,3] and returns it
 # Example: count_positives([1,6,-4,-2,-7,-2]) changes the list to [1,6,-4,-2,-7,2] and returns it
 # NOT DONE
-# list = [1,6,-4,-2,-7,-2]
-# def count_positives():
-#     num = 0
-#     new_list = []
-#     for i in range(len(list)):
-#         print(len(list))
-#         if num in list < 0:
-#             new_list.append(list[i])
-#             print(new_list)
-#         return len(new_list)
+list = [1,6,-4,-2,-7,-2]
+def count_positives(list):
+    new_list = []
+    for num in list:
+        if num in list[i] < 0:
+            new_list.append(num)
+            print(new_list)
+    
+    
+print(count_positives(list))
+    
+    # num = 0
+    # new_list = []
+    # for i in range(len(list)):
+    #     print(len(list))
+    #     if num in list < 0:
+    #         new_list.append(list[i])
+    #         # print(new_list)
+    #     return len(new_list)
       
 
 # print(count_positives())
@@ -72,25 +81,47 @@
 # Example: minimum([37,2,1,-9]) should return -9
 # Example: minimum([]) should return False
 
-def minimum(list):
-  min_num = list[0]  
-  for i in list:       
-    if i < min_num:
-      min_num = i  
-  return min_num
+# list=[37,2,1,-9]
+# def minimum(list):
+#     min_num = list[0]
+#     for i in list:       
+#         if i < min_num:
+#             min_num = i
+#     return min_num
 
-print(minimum([37,2,1,-9])) 
-
+# print(minimum(list))
+ 
+# print(minimum([])) 
 
 
 # 7. Maximum - Create a function that takes a list and returns the maximum value in the list. If the list is empty, have the function return False.
 # Example: maximum([37,2,1,-9]) should return 37
 # Example: maximum([]) should return False
 
+# def minimum(list):
+#     min_num = list[0]
+#     for i in list:       
+#         if i > min_num:
+#             min_num = i   
+#         # else:
+#         #     min_num == 0
+#         #     return False  
+#     return min_num
+
+ 
+# # print(minimum([])) 
+# print(minimum([37,2,1,-9]) )
+
 
 # 8. Ultimate Analysis - Create a function that takes a list and returns a dictionary that has the sumTotal, average, minimum, maximum and length of the list.
 # Example: ultimate_analysis([37,2,1,-9]) should return {'sumTotal': 31, 'average': 7.75, 'minimum': -9, 'maximum': 37, 'length': 4 }
 
+# def ultimate_analysis():
+
+
 
 # 9. Reverse List - Create a function that takes a list and return that list with values reversed. Do this without creating a second list. (This challenge is known to appear during basic technical interviews.)
 # Example: reverse_list([37,2,1,-9]) should return [-9,1,2,37]
+
+# def reverse_list():
+#     for i in range( len(list))
