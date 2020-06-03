05f9a789e27d910500c5ebdc280dbb90c73c1619
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-06-03 02:05:51 -0700

    done

--- python_stack/python/OOP/attributes.py
+++ python_stack/python/OOP/attributes.py
@@ -10,11 +10,3 @@ monty = User("Monty Python", "monty@python.com")
 print(guido.name)
 print(monty.name)
 
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account = 0
-
-    def make_deposit(self, amount):
-        self.account_balance += amount
